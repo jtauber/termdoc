ecc8f56470d1eaecd3a1ce74c1ca1f13ed410749
James Tauber <jtauber@jtauber.com>
2021-05-21 07:14:28 +0800

    initial setuptools setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="termdoc",
+    version="0.1",
+    description="Python library and tools for working with term-document matrices",
+    url="http://github.com/jtauber/termdoc",
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    license="MIT",
+    packages=["termdoc"],
+)
