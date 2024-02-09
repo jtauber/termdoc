fdc3a82419c7e2abcdc0c7f066927d44a67aed23
James Tauber <jtauber@jtauber.com>
2024-02-09 06:42:25 -0500

    added long description

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+from pathlib import Path
 from setuptools import setup
+
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 
 setup(
     name="termdoc",
     version="0.3",
     description="Python library and tools for working with term-document matrices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="http://github.com/jtauber/termdoc",
     author="James Tauber",
     author_email="jtauber@jtauber.com",
